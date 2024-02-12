b38e96e15df208c2df9889b0ffed468df7d9d97d
Sakshi Mandlecha <sakshi.mandlecha298@gmail.com>
2024-02-12 13:11:22 +0530

    DataSourceController

--- ExportUtility/src/main/java/com/maiara/report_utility_host/controller/DatabaseSourceController.java
+++ ExportUtility/src/main/java/com/maiara/report_utility_host/controller/DatabaseSourceController.java
@@ -1,17 +1,23 @@
 package com.maiara.report_utility_host.controller;
 
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.maiara.report_utility.database.entities.DatabaseSource;
+import com.maiara.report_utility.errors.DataNotFoundException;
 import com.maiara.report_utility.host.service.ExportUtility;
 import com.maiara.report_utility.host.service.params.IDatabaseSource;
 
 @RestController
-@RequestMapping("/database")
+@RequestMapping("/datasource")
 public class DatabaseSourceController {
 	String name;
 	String driverClass;
@@ -19,21 +25,28 @@ public class DatabaseSourceController {
 	
 	@Autowired
 	ExportUtility export = new ExportUtility();
-	IDatabaseSource dbsource;
 	
-	@PostMapping("/add")
-	public void addSource(@RequestBody DatabaseSource obj) {
-		name = obj.getName();
-		driverClass = obj.getDriverClass();
-		sourceName = obj.getSourceName();
-	    obj.setName(name);
-		obj.setDriverClass(driverClass);
-		obj.setSourceName(sourceName);
-		export.databaseSource.add(dbsource);
 	
-		
-		
-		
-	}
+	@GetMapping("/get")
+	public ResponseEntity<?> getDatabaseSources(@RequestParam (required = false) Long id){
+		if(id!=null) {
+			IDatabaseSource record = null;
+			try {
+				record = export.databaseSource.get(id);
+				return ResponseEntity.ok(record);
+			} catch (DataNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return ResponseEntity.notFound().build();
+			}
+			
+		}
+		else{
+			List<IDatabaseSource> datasources = export.databaseSource.get();
+			return ResponseEntity.ok(datasources);
+			
 
+		}
+	}
 }
+		
